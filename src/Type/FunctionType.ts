495c4af5f8273f0f57737817ccec161c3ec9d69e
Adrian <6987180+adr-h@users.noreply.github.com>
2025-08-10 02:57:26 +0800

    get the full text of the jsdoc

--- src/Type/FunctionType.ts
+++ src/Type/FunctionType.ts
@@ -10,23 +10,10 @@ export class FunctionType extends BaseType {
         protected namedArguments?: ObjectType,
     ) {
         super();
-
         if (node) {
             this.comment = getJSDocCommentsAndTags(node).map(
-                node => {
-                    if (!isJSDoc(node)) {
-                        return '';
-                    }
-
-                    let comment = node.comment || '';
-                    if (!node.tags) {
-                        return comment;
-                    }
-                    return comment + '\n' + node.tags.map(tag => `@${tag.tagName.getText()}: ${tag.comment}`).join('\n')
-                }
+                node => node.getFullText()
             ).join(' ')
-
-            // this.comment = `(${getJSDocTags(node)} ${node.parameters.map((p) => p.getFullText()).join(",")}) =>${node.type?.getFullText()}`;
         }
     }
 
